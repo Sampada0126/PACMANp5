7334ab219e3d8db5b147cce3f5e0c0e3d32bdbc1
Sampada0126 <67427222+Sampada0126@users.noreply.github.com>
2020-07-31 10:27:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function setup() {
     frameRate(8);
     
     //create link function
-   link = createA("https://akanksha-02.github.io/pacman.github.io/", 
+   link = createA("https://sampada0126/PACMAN/", 
                        "click", "_blank");
 
    // Posotion the anchor objects 
